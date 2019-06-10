416923470f51206edf7836e0425fcf7126a147d6
Nikita Kiselov <kinivi210999@gmail.com>
2019-06-10 14:15:43 -0400

    Adding NLLLoss

--- main.py
+++ main.py
@@ -57,9 +57,32 @@ class my_sensors(data.Dataset):
         return torch.Tensor(self.data[index]), torch.Tensor(self.target[index])
 
 
+class my_sensors_NNLL(data.Dataset):
+    def __init__(self, input_data):
+        self.data = input_data[:, :23]  # 1st and 2nd columns --> x,y
 
-training_data = my_sensors(tr_data)
-test_data = my_sensors(test_data)
+        temp_arr = input_data[:, 23:]
+        final_arr = []
+
+        # Reshaping array to get normalized feature vector
+        for x in temp_arr:
+            final_arr.append(int(x[0]))
+
+        self.target = np.asarray(final_arr)
+        print(self.target[0])
+        self.n_samples = self.data.shape[0]
+
+
+    def __len__(self):  # Length of the dataset.
+        return self.n_samples
+
+    def __getitem__(self, index):  # Function that returns one point and one label.
+        #print(torch.Tensor([self.target[index]]))
+        return torch.Tensor(self.data[index]), torch.Tensor([self.target[index]])
+
+
+training_data = my_sensors_NNLL(tr_data)
+test_data = my_sensors_NNLL(test_data)
 
 
 # We create the dataloader for both data
@@ -79,6 +102,7 @@ class my_model(nn.Module):
             nn.Linear(64, 64),
             nn.Linear(64, 64),
             nn.Linear(64, self.n_out),
+            nn.(dim=1),
         )
 
     def forward(self, x):
@@ -93,30 +117,41 @@ class my_model(nn.Module):
 model = my_model()
 
 # Negative log likelihood loss.
-criteria = nn.MSELoss()
+criteria = nn.NLLLoss()
 
 # Adam optimizer with learning rate 0.1 and L2 regularization with weight 1e-4.
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-4)
 
 # Training
-print("----- Begin training-----")
+print("----- Begin training -----")
 
 for epoch in range(9):
 
     running_loss = 0.0
     for k, (sensors_data, target) in enumerate(my_loader):
 
+        # Resizing array for NLLLoss
+        final_arr = []
+
+        # Reshaping array to get normalized feature vector
+        for x in target:
+            final_arr.append(int(x[0]))
+
+        target = torch.Tensor(np.asarray(final_arr))
+
         # Definition of inputs as variables for the net.
         # requires_grad is set False because we do not need to compute the
         # derivative of the inputs.
         sensors_data = Variable(sensors_data, requires_grad=False)
-        target = Variable(target.float(), requires_grad=False)
+        target = Variable(target.long(), requires_grad=False)
+        target_NLLL = Variable(torch.FloatTensor(500).uniform_(0, 12).long())
 
-        # Set gradient to 0.
+        # Set gradient to 0
         optimizer.zero_grad()
         # Feed forward.
         pred = model(sensors_data)
         # Loss calculation.
+
 
         loss = criteria(pred, target)
         # Gradient calculation.
@@ -134,6 +169,25 @@ for epoch in range(9):
         optimizer.step()
 
     print(running_loss)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Test
 print("---- Begin test ----")
@@ -162,18 +216,12 @@ for k, (sensors_data, target) in enumerate(my_loader_2):
 
     correct += (target_indieces == prediction_indieces).sum().item()
 
-
     # Loss calculation.
     loss = criteria(pred, target)
     # Gradient calculation.
     loss.backward()
 
     running_loss += loss.item()
-    # Print loss every 10 iterations.
-    if k % 1000 == 0:
-        print('[%d, %5d] loss: %.8f' %
-              (epoch + 1, k + 1, loss))
-        running_loss = 0.0
 
 
 print(running_loss)

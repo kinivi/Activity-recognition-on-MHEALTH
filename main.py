cfbb26e7f633d306f5bfc4a51fdced1b9eabaa05
Nikita Kiselov <kinivi210999@gmail.com>
2019-06-10 14:18:50 -0400

    Fixed Requirements

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # Lets start with the imports.
 import numpy as np
 import pandas as pd
-from PIL import Image
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
